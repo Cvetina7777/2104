23ed8c83a71baf8de2572cc6642a999b8552942c
Cvetina7777 <tt70207252@edu1.pgzaimov.com>
2024-04-20 13:09:52 +0300

    Domashna

---
+++ ListOfStaff/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfStaff
+{
+    public class Employee
+    {
+        public string EmployeeName
+        {
+            get
+            {
+                return EmployeeName;
+            }
+          set { EmployeeName = value; }
+        }
+        
+        public int countofStaff {  get; set; }
+
+
+        
+
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+    
+    
+      private List<Employee> staff = new List<Employee>();
+        public List<Employee> Staff
+        {
+            get
+            {
+                return staff;
+
+            }
+            set 
+            { 
+            staff = value;
+            }
+            
+        }
+        public void PrintAll()
+        {
+            Console.WriteLine($"EmployeeName{this.EmployeeName}");
+            foreach ( Employee emp in staff )
+            {
+                
+            }
+        }
+    }
+    
+   
+
+    
+   
+
+}
---
+++ ListOfStaff/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace ListOfStaff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee employee = new Employee();
+          Console.WriteLine("input count of staff");
+            Employee staff = new Employee();
+                Employee staffEmployee = new Employee();
+                Console.Write("input name: ");
+                staffEmployee.EmployeeName = Console.ReadLine();
+                Console.WriteLine("input Salary");
+                staffEmployee.Salary = double.Parse(Console.ReadLine());
+                 Console.WriteLine("input Position"); 
+                staffEmployee.Position = Console.ReadLine();
+                Console.WriteLine("input email");
+                staffEmployee.Email = Console.ReadLine();
+                 Console.WriteLine("input age");
+                staffEmployee.Age=int.Parse(Console.ReadLine());
+                employee.Staff.Add(staffEmployee);
+            
+            
+           
+           
+        }
+    }
+}
